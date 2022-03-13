8ca1a03197b5c33ed8cd7ec819a6a55c8d60cc72
sulaymon333 <tjsulaymon@gmail.com>
2022-03-13 23:46:53 +0200

    added data

---
+++ root/js/data.js
@@ -0,0 +1,32 @@
+export const quizData = [
+    {
+        id: 1,
+        question: 'What is the capital of France?',
+        answers: ['Paris', 'London', 'Berlin', 'Madrid'],
+        correctAnswer: 'Paris',
+    },
+    {
+        id: 2,
+        question: 'What is the capital of Germany?',
+        answers: ['Paris', 'London', 'Berlin', 'Madrid'],
+        correctAnswer: 'Berlin',
+    },
+    {
+        id: 3,
+        question: 'What is the capital of Italy?',
+        answers: ['Rome', 'London', 'Berlin', 'Madrid'],
+        correctAnswer: 'Rome',
+    },
+    {
+        id: 4,
+        question: 'What is the capital of Spain?',
+        answers: ['Paris', 'London', 'Berlin', 'Madrid'],
+        correctAnswer: 'Madrid',
+    },
+    {
+        id: 5,
+        question: 'What is the capital of Portugal?',
+        answers: ['Paris', 'Lisbon', 'Berlin', 'Madrid'],
+        correctAnswer: 'Lisbon',
+    },
+];
